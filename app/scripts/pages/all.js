d9b63aafac81e397298f930d1f365b043d2dbed8
OPTICALEXPRESS\Andrew_Sh <a0andy@live.com>
2017-08-14 15:22:50 +0100

    header/nav started

--- app/scripts/pages/all.js
+++ app/scripts/pages/all.js
@@ -1,7 +1,12 @@
 (function($, window, document) {
 
-
+  function mainnav() {
+    $('.menu').on('click', function() {
+      $(this).children().toggleClass('open');
+    });
+  }
 
  $(function() {
+   mainnav();
  });
 }(window.jQuery, window, document));
