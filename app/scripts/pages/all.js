d6130d0b0fad63f354379ea8675e86d4b63cf341
OPTICALEXPRESS\Andrew_Sh <a0andy@live.com>
2017-08-16 15:20:04 +0100

    added reservation form

--- app/scripts/pages/all.js
+++ app/scripts/pages/all.js
@@ -1,8 +1,30 @@
 (function($, window, document) {
 
   function mainnav() {
+    $( "#datepicker" ).datepicker({
+      changeMonth: true,
+      changeYear: true,
+      minDate: 0,
+      maxDate: "+6M +10D",
+      dateFormat: 'dd-mm-yy'
+    });
+
     $('header .menu').on('click', function() {
-      $('header, nav').stop().toggleClass('open');
+      $('.reservationform').stop().fadeOut();
+      $('.reservationbtn').stop().removeClass('open');
+
+      $('header').stop().toggleClass('open');
+      $('nav').stop().fadeToggle();
+    });
+
+    $('.reservationbtn').on('click', function(e) {
+      $('nav').stop().fadeOut();
+      $('header').stop().removeClass('open');
+
+      $('.reservationform').stop().fadeToggle();
+      $('.reservationbtn').stop().toggleClass('open');
+
+      e.preventDefault();
     });
 
     $('nav p').on('click', function() {
@@ -10,13 +32,14 @@
       $(this).next('.sub').stop().slideToggle();
     });
 
-    $('header, nav').on('click', function(e) {
+    $('header, nav, .reservationform, .ui-datepicker').on('click', function(e) {
       e.stopPropagation();
     });
 
     $('html').on('click', function() {
-      $('header, nav').stop().removeClass('open');
+      $('header, .reservationbtn').stop().removeClass('open');
       $('.sub').stop().slideUp();
+      $('nav, .reservationform').stop().fadeOut();
     });
 
     $(window).on('scroll', function() {
